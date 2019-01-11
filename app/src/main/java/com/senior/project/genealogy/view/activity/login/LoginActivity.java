f99c74aaeb86bb5c90578d6901f7743f71e29600
lorence <lorence@enclave.vn>
2019-01-11 10:19:37 +0700

    Add .gitingore

--- app/src/main/java/com/senior/project/genealogy/view/activity/login/LoginActivity.java
+++ app/src/main/java/com/senior/project/genealogy/view/activity/login/LoginActivity.java
@@ -1,16 +1,24 @@
 package com.senior.project.genealogy.view.activity.login;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -18,6 +26,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.senior.project.genealogy.R;
 import com.senior.project.genealogy.response.User;
 import com.senior.project.genealogy.util.Constants;
+import com.senior.project.genealogy.util.GaussianBlur;
 import com.senior.project.genealogy.util.OverrideFonts;
 import com.senior.project.genealogy.util.Utils;
 import com.senior.project.genealogy.view.activity.register.RegisterActivity;
@@ -41,8 +50,15 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
     @BindView(R.id.lnSignup)
     LinearLayout lnSignup;
 
+    @BindView(R.id.container)
+    ImageView container;
+
+    @BindView(R.id.layout)
+    LinearLayout layout;
+
     private LoginPresenterImpl loginPresenterImpl;
     private ProgressDialog mProgressDialog;
+    private GaussianBlur blur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,13 +66,46 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
         setContentView(R.layout.activity_login);
         FirebaseMessaging.getInstance().subscribeToTopic(Utils.getDeviceId());
         ButterKnife.bind(this);
-        initFonts();
-        loginPresenterImpl = new LoginPresenterImpl(this);
+        initComponents();
     }
 
-    private void initFonts() {
+    private void initComponents() {
         edtUsername.setTypeface(OverrideFonts.getTypeFace(this, OverrideFonts.TYPE_FONT_NAME.HELVETICANEUE, OverrideFonts.TYPE_STYLE.LIGHT));
         edtPassword.setTypeface(OverrideFonts.getTypeFace(this, OverrideFonts.TYPE_FONT_NAME.HELVETICANEUE, OverrideFonts.TYPE_STYLE.LIGHT));
+        loginPresenterImpl = new LoginPresenterImpl(this);
+        container.setVisibility(View.GONE);
+        blur = new GaussianBlur(LoginActivity.this);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            setStatusBarGradiant(this);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        container.setVisibility(View.VISIBLE);
+
+        layout.setDrawingCacheEnabled(true);
+        layout.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_LOW);
+
+        Bitmap bitmap = layout.getDrawingCache();
+
+        container.setImageBitmap(blur.gaussianBlur(25, bitmap));
+
+        layout.setVisibility(View.VISIBLE);
+    }
+
+    @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.M)
+    public static void setStatusBarGradiant(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            Drawable background = activity.getResources().getDrawable(R.drawable.custom_bg);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.setNavigationBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            window.setBackgroundDrawable(background);
+        }
     }
 
     @OnTextChanged({R.id.username, R.id.password})
@@ -73,6 +122,7 @@ public class LoginActivity extends AppCompatActivity implements LoginView {
             btnLogin.setEnabled(true);
         }
     }
+
     @OnClick({R.id.btnLogin, R.id.lnSignup})
     public void onClick(View view)
     {
